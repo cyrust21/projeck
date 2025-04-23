262169c4d01a77642cf24099e4e7b64f64148593
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2025-04-23 23:31:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/ffstalk/:uid', async (req, res) => {
 })
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
