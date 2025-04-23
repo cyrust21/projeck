a029a9c66c0b8ad706deebc7a827f7a652ae1c03
Cyrust Mods <45724743+cyrust21@users.noreply.github.com>
2025-04-23 19:53:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express')
-const ffStalk = require('./ffStalk')
 const cors = require('cors')
-const app = express()
 
+const app = express()
 app.use(cors())
 
-app.get('/', (req, res) => res.send('FFStalk API is Running!'))
-
-app.get('/api/stalk/:uid', async (req, res) => {
-  try {
-    const data = await ffStalk(req.params.uid)
-    res.json(data)
-  } catch (err) {
-    res.status(500).json({ error: err.message || 'Fetch error' })
-  }
+app.get('/', (req, res) => {
+  res.send('Hello from Railway!')
 })
 
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Running on port ${PORT}`))
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
